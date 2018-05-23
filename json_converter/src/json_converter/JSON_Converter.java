e907c6be528befae7aa013cf06978ea95e2cc63a
Collaboratore2018 <Collaboratore2018@DESKTOP-D1CAVGE>
2018-05-23 12:26:45 +0200

    Fixed GUI

--- json_converter/src/json_converter/JSON_Converter.java
+++ json_converter/src/json_converter/JSON_Converter.java
@@ -2,10 +2,13 @@ package json_converter;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+
 import javax.swing.*;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.UIManager.*;
+import javax.swing.filechooser.FileFilter;
 
 public class JSON_Converter{
 
@@ -30,14 +33,44 @@ public class JSON_Converter{
 				lblJsonConverter.setHorizontalAlignment(SwingConstants.CENTER);
 				lblJsonConverter.setFont(new Font("SansSerif", Font.BOLD, 17));
 				
+				JButton btnConvert = new JButton("Convert");
+				
+				JLabel lblNewLabel = new JLabel("Selected file Path:");
+				
+				JLabel lblNewLabel_1 = new JLabel("Selected file Name:");
+				
+				JLabel lblPath = new JLabel("");
+				
+				JLabel lblName = new JLabel("");
+				
 				JButton btnExit = new JButton("Exit");
 				
 				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setFileFilter(new FileFilter() {
+
+					   public String getDescription() {
+					       return "JSON Files (*.json)";
+					   }
+
+					   public boolean accept(File f) {
+					       if (f.isDirectory()) {
+					           return true;
+					       } else {
+					           String filename = f.getName().toLowerCase();
+					           return filename.endsWith(".json") || filename.endsWith(".json") ;
+					       }
+					   }
+					});
+				 fileChooser.addActionListener(new ActionListener() {
+				      public void actionPerformed(ActionEvent e) {
+				    	  lblPath.setText(fileChooser.getSelectedFile().getAbsolutePath());
+				    	  lblName.setText(fileChooser.getSelectedFile().getName());
+				      }
+				    });
 				
 				JLabel lblSelezionaIlFile = new JLabel("Select the file you want to convert:");
 				lblSelezionaIlFile.setFont(new Font("Arial", Font.PLAIN, 14));
 				
-				JButton btnConvert = new JButton("Convert");
 				GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
 				groupLayout.setHorizontalGroup(
 					groupLayout.createParallelGroup(Alignment.LEADING)
@@ -49,14 +82,27 @@ public class JSON_Converter{
 								.addGroup(groupLayout.createSequentialGroup()
 									.addContainerGap()
 									.addComponent(btnConvert)
-									.addPreferredGap(ComponentPlacement.RELATED, 412, Short.MAX_VALUE)
+									.addPreferredGap(ComponentPlacement.RELATED, 432, Short.MAX_VALUE)
 									.addComponent(btnExit, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE))
 								.addGroup(groupLayout.createSequentialGroup()
 									.addGap(199)
-									.addComponent(lblJsonConverter, GroupLayout.PREFERRED_SIZE, 181, GroupLayout.PREFERRED_SIZE))
+									.addComponent(lblJsonConverter, GroupLayout.DEFAULT_SIZE, 181, Short.MAX_VALUE)
+									.addGap(198))
 								.addGroup(groupLayout.createSequentialGroup()
 									.addContainerGap()
-									.addComponent(fileChooser, GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)))
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(fileChooser, GroupLayout.DEFAULT_SIZE, 572, Short.MAX_VALUE)
+										.addGroup(groupLayout.createSequentialGroup()
+											.addGap(6)
+											.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+												.addGroup(groupLayout.createSequentialGroup()
+													.addComponent(lblNewLabel_1)
+													.addPreferredGap(ComponentPlacement.RELATED)
+													.addComponent(lblName))
+												.addGroup(groupLayout.createSequentialGroup()
+													.addComponent(lblNewLabel)
+													.addPreferredGap(ComponentPlacement.RELATED)
+													.addComponent(lblPath)))))))
 							.addContainerGap())
 				);
 				groupLayout.setVerticalGroup(
@@ -67,7 +113,15 @@ public class JSON_Converter{
 							.addComponent(lblSelezionaIlFile)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(fileChooser, GroupLayout.PREFERRED_SIZE, 261, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.RELATED, 143, Short.MAX_VALUE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblNewLabel)
+								.addComponent(lblPath))
+							.addPreferredGap(ComponentPlacement.UNRELATED)
+							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+								.addComponent(lblNewLabel_1)
+								.addComponent(lblName))
+							.addPreferredGap(ComponentPlacement.RELATED, 93, Short.MAX_VALUE)
 							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 								.addComponent(btnExit)
 								.addComponent(btnConvert))
@@ -86,5 +140,6 @@ public class JSON_Converter{
 	            });
 	        }
 	    });
+	    
 	}
 }
